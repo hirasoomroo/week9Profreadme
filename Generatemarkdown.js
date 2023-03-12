9871aa5a79c2fd76028ac0e4b84bd00e33282453
Hira Soomro <hira@Hiras-MacBook-Pro.local>
2023-03-12 16:17:48 -0700

    commiting changes

---
+++ Generatemarkdown.js
@@ -0,0 +1,62 @@
+// TODO: Create a function that returns a license badge based on which license is passed in
+// If there is no license, return an empty string
+function renderLicenseBadge(license) {}
+//function to generate the markdown here
+function generateMarkdown(data) {
+    return `
+# Project Title
+${data.title}
+${data.getLicense}
+
+# Description
+${data.description}
+
+# Table of Contents 
+* [Installation](#-Installation)
+* [Usage](#-Usage)
+* [License](#-Installation)
+* [Contributing](#-Contributing)
+* [Tests](#-Tests)
+* [Questions](#-Contact-Information)
+    
+# Installation
+${data.installation}
+
+# Usage
+${data.usage}
+
+# License 
+${data.license}
+* As this license list was not comprehensive, if you need another license, use the contact information below to ask for license to be added. 
+
+# Contributing 
+${data.contributing}
+
+# Tests
+${data.tests}
+
+# Contact Information 
+* GitHub Username: ${data.userName}
+* Contact Email: ${data.userEmail}
+
+`;
+}
+
+//need to export the generateMarkdown function 
+module.exports = generateMarkdown;
+// TODO: Create a function that returns the license link
+// If there is no license, return an empty string
+function renderLicenseLink(license) {}
+
+// TODO: Create a function that returns the license section of README
+// If there is no license, return an empty string
+function renderLicenseSection(license) {}
+
+// TODO: Create a function to generate markdown for README
+function generateMarkdown(data) {
+  return `# ${data.title}
+
+`;
+}
+
+module.exports = generateMarkdown;
